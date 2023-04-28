68e5642fa3feb669af7c99c20f983b1b66f475e6
Dashch <dashch88@gmail.com>
2023-04-28 13:18:10 +0400

    Added Task 38 at Homework 8

---
+++ Python_HW/Python_HW8/file_proc.py
@@ -0,0 +1,23 @@
+# Функция добавления заиси в файл
+def write_contact(record):
+    phonebook = open('Python_HW/Python_HW8/phonebook.txt', 'a')
+    phonebook.writelines(record)
+    phonebook.close()
+    return
+
+# Функция вывода в список всего файла
+def read_phonebook():
+    all_rec = []
+    phonebook = open('Python_HW/Python_HW8/phonebook.txt', 'r')
+    all_rec = phonebook.readlines()
+    phonebook.close()
+    return all_rec
+
+
+# Перезапись всего файла
+def rewrite_phonebook(all_rec):
+    phonebook = open('Python_HW/Python_HW8/phonebook.txt', 'w')
+    for record in all_rec:
+        phonebook.writelines(record)
+    phonebook.close()
+    return
